6648dac6bca557271a90a60c0620e09922a63a4a
kkoon9 <rndrnjs2003@naver.com>
2019-11-22 05:55:03 +0900

    update earn

--- model/earn.js
+++ model/earn.js
@@ -20,14 +20,8 @@ module.exports = {
         });
         return;
       }
-<<<<<<< HEAD
       const groupIdx = await pool.queryParam_None(`SELECT groupIdx FROM groups WHERE name='${name}'`);
       console.log(groupIdx);
-=======
-      const groupIdx = await pool.queryParam_None(
-        `SELECT groupIdx FROM groups WHERE name='${name}'`
-      );
->>>>>>> 443becd08223bd6ac06ec6dc4a6779cb3d1fe4a0
       if (!groupIdx) {
         resolve({
           code: statusCode.BAD_REQUEST,
@@ -35,13 +29,8 @@ module.exports = {
         });
         return;
       }
-<<<<<<< HEAD
       console.log(groupIdx[0].groupIdx);
       const updateQuery = `UPDATE ${table} SET mandarins=mandarins+1 WHERE groupIdx='${groupIdx[0].groupIdx}' AND userIdx='${userIdx[0].userIdx}'`;
-=======
-      console.log(groupIdx, userIdx);
-      const updateQuery = `UPDATE ${table} SET mandarins=mandarins+1 WHERE groupIdx=${groupIdx[0].groupIdx} AND userIdx=${userIdx[0].userIdx}`;
->>>>>>> 443becd08223bd6ac06ec6dc4a6779cb3d1fe4a0
       const updateResult = await pool.queryParam_None(updateQuery);
       const currentMandarin = await pool.queryParam_None(
         `SELECT mandarins FROM user_groups WHERE id='${id}'`

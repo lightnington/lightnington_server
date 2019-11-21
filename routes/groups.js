53ac2ca388c5a1fa7f52f4ddc4705817e8dd001b
양정훈 <luciferkala@gmail.com>
2019-11-21 22:45:56 +0900

    making users

--- routes/groups.js
+++ routes/groups.js
@@ -3,18 +3,16 @@ const router = express.Router();
 const pool = require("../module/poolAsync");
 const responseMessage = require("../module/util/responseMessage");
 const statusCode = require("../module/util/statusCode");
-const utils = require("../module/util/utils");
+
 /* GET users listing. */
-router.post("/signin", (req, res) => {
-  const { id, pwd } = req.body;
-  const query = `SELECT * FROM user WHERE id="${id}" AND pwd="${pwd}"`;
+router.post("/groupin", (req, res) => {
+  const { name } = req.body;
+  const query = `SELECT * FROM group WHERE name="${name}"`;
   const value = [id, pwd];
 
   if (!id || !pwd) {
     //id pwd 비어 있는 경우
-    res
-      .status(statusCode.FORBIDDEN)
-      .send(utils.successFalse(responseMessage.NULL_VALUE));
+    res.status(statusCode.FORBIDDEN).send(responseMessage.NULL_VALUE);
   }
   pool
     .queryParam_Parse(query, value)
@@ -25,23 +23,17 @@ router.post("/signin", (req, res) => {
     .catch(err => console.log(err));
 });
 
-router.post("/signup", function(req, res, next) {
-  const { id, pwd, phone } = req.body;
-  const insertQuery = "INSERT INTO user (id, pwd, phone) values (?, ?, ?);";
-  const insertValue = [id, pwd, phone];
+router.post("/groupup", function(req, res, next) {
+  const { name } = req.body;
+  const query = "INSERT INTO user (name) values (?);";
+  const value = [name];
 
-  //회원가입 정보가 기재 안 되어 있을때
-  if (!id || !pwd || !phone) {
+  if (!name) {
     res.status(statusCode.NO_CONTENT).send(responseMessage.SIGN_UP_FAIL);
   }
 
-  const selectQuery = `SELECT * FROM user WHERE="${id}"`;
-  /*
-  id 중복 체크 필요 
-  */
-
   pool
-    .queryParam_Parse(insertQuery, insertValue)
+    .queryParam_Parse(query, value)
     .then(data => {
       console.log(data);
       res.status(statusCode.OK).send(responseMessage.SIGN_UP_SUCCESS);

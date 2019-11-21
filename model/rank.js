35844e7f15c988798b88ee30d4c11898c2284202
kkoon9 <rndrnjs2003@naver.com>
2019-11-22 01:36:13 +0900

    update rank

--- model/rank.js
+++ model/rank.js
@@ -8,21 +8,21 @@ module.exports = {
         groupIdx
     }) => {
         const table = 'user_groups';
-        let query = `SELECT mandarins, userIdx FROM ${table} WHERE groupIdx='${groupIdx}';`;
+        const query = `SELECT mandarins, userIdx FROM ${table} WHERE groupIdx=${groupIdx};`;
         const message = responseMessage.RANK_READ_SUCCESS;
         return new Promise(async (resolve, reject) => {
-            const idxResult = await pool.queryParam_None(query);
-            console.log(idxResult);
-            if (!idxResult) {
+            const result = await pool.queryParam_None(query);
+            console.log(result);
+            if (!result) {
                 resolve({
                     code: statusCode.NOT_FOUND,
-                    json: authUtil.successFalse(responseMessage.RANK_READ_FAIL)
+                    json: utils.successFalse(responseMessage.RANK_READ_FAIL)
                 });
                 return;
             }
             resolve({
                 code: statusCode.OK,
-                json: authUtil.successTrue(message, idxResult)
+                json: utils.successTrue(message, result)
             });
         });
     },

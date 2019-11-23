26c474cca3b52cd8af4fa30793e4ae6b2d864dca
kkoon9 <rndrnjs2003@naver.com>
2019-11-23 17:19:56 +0900

    update main.js

--- routes/main.js
+++ routes/main.js
@@ -1,30 +1,73 @@
 var express = require('express');
 var router = express.Router();
-const resMessage = require("../module/util/responseMessage");
-const statusCode = require("../module/util/statusCode");
-const util = require("../module/util/utils");
-router.get('/',  async(req, res) => {
+const util = require('../module/utils');
+const statusCode = require('../module/statusCode');
+const resMessage = require('../module/responseMessage');
+
+router.get('/' , async(req, res) => {
     //전체
     const result=[];
     //favorite
     const favorite = [];
-    var favorite_json = new Object();
-     favorite_json.image = "https://sopt-kkoon9.s3.ap-northeast-2.amazonaws.com/1574495684431.jpg";
-    favorite_json.name = "test1";
-    favorite.push(favorite_json);
+    var favorite_json_1 = new Object();
+    favorite_json_1.image = "https://sopt-kkoon9.s3.ap-northeast-2.amazonaws.com/1574495684431.jpg";
+    favorite_json_1.name = "test1";
+    var favorite_json_2 = new Object();
+    favorite_json_2.image = "https://sopt-kkoon9.s3.ap-northeast-2.amazonaws.com/1574495684431.jpg";
+    favorite_json_2.name = "test1";
+    var favorite_json_3 = new Object();
+    favorite_json_3.image = "https://sopt-kkoon9.s3.ap-northeast-2.amazonaws.com/1574495684431.jpg";
+    favorite_json_3.name = "test1";
+    var favorite_json_4 = new Object();
+    favorite_json_4.image = "https://sopt-kkoon9.s3.ap-northeast-2.amazonaws.com/1574495684431.jpg";
+    favorite_json_4.name = "test1";
+    favorite.push(favorite_json_1);
+    favorite.push(favorite_json_2);
+    favorite.push(favorite_json_3);
+    favorite.push(favorite_json_4);
+
     //beauty
     const beauty = [];
-    var beauty_json = new Object();
-     beauty_json.image = "https://sopt-kkoon9.s3.ap-northeast-2.amazonaws.com/1574494559696.jpg";
-    beauty_json.brand = "test1";
-    beauty_json.product = "test1";
-    beauty_json.price = "200";
-    beauty.push(beauty_json);
+    var beauty_json_1 = new Object();
+    beauty_json_1.image = "https://sopt-kkoon9.s3.ap-northeast-2.amazonaws.com/1574494559696.jpg";;
+    beauty_json_1.brand = "test1";
+    beauty_json_1.product = "test1";
+    beauty_json_1.price = "200won";
+    var beauty_json_2 = new Object();
+    beauty_json_2.image = "https://sopt-kkoon9.s3.ap-northeast-2.amazonaws.com/1574494559696.jpg";;
+    beauty_json_2.brand = "test1";
+    beauty_json_2.product = "test1";
+    beauty_json_2.price = "200won";
+    var beauty_json_3 = new Object();
+    beauty_json_3.image = "https://sopt-kkoon9.s3.ap-northeast-2.amazonaws.com/1574494559696.jpg";;
+    beauty_json_3.brand = "test1";
+    beauty_json_3.product = "test1";
+    beauty_json_3.price = "200won";
+    var beauty_json_4 = new Object();
+    beauty_json_4.image = "https://sopt-kkoon9.s3.ap-northeast-2.amazonaws.com/1574494559696.jpg";;
+    beauty_json_4.brand = "test1";
+    beauty_json_4.product = "test1";
+    beauty_json_4.price = "200won";
+    beauty.push(beauty_json_1);
+    beauty.push(beauty_json_2);
+    beauty.push(beauty_json_3);
+    beauty.push(beauty_json_4);
+
     //ad
     const ad = [];
-    var ad_json = new Object();
-     ad_json.image = "https://sopt-kkoon9.s3.ap-northeast-2.amazonaws.com/1574495385381.jpg";
-    ad.push(ad_json);
+    var ad_json_1 = new Object();
+    var ad_json_2 = new Object();
+    var ad_json_3 = new Object();
+    var ad_json_4 = new Object();
+    ad_json_1.image = "https://sopt-kkoon9.s3.ap-northeast-2.amazonaws.com/1574495385381.jpg";
+    ad_json_2.image = "https://sopt-kkoon9.s3.ap-northeast-2.amazonaws.com/1574495385381.jpg";
+    ad_json_3.image = "https://sopt-kkoon9.s3.ap-northeast-2.amazonaws.com/1574495385381.jpg";
+    ad_json_4.image = "https://sopt-kkoon9.s3.ap-northeast-2.amazonaws.com/1574495385381.jpg";
+
+    ad.push(ad_json_1);
+    ad.push(ad_json_2);
+    ad.push(ad_json_3);
+    ad.push(ad_json_4);
 
     var json = new Object();
     json.favorite = favorite;
